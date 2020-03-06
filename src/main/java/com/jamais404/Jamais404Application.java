5389de9d6b5e31a63aee43a428ef4ddae142996e
loic.jurasz <loic.jurasz@he-arc.ch>
2020-03-06 09:02:13 +0100

    base

--- src/main/java/com/jamais404/Jamais404Application.java
+++ src/main/java/com/jamais404/Jamais404Application.java
@@ -1,9 +1,10 @@
-package com.example.demo;
+package com.jamais404;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class Jamais404Application {
 
 	public static void main(String[] args) {
