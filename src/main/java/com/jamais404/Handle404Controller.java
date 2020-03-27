0f62bc6f68ed2fdb95335a876aa1c7b9a207f84c
qtipee <quent.vallat@gmail.com>
2020-03-27 09:05:35 +0100

    Handle404Controller

--- src/main/java/com/jamais404/Handle404Controller.java
+++ src/main/java/com/jamais404/Handle404Controller.java
@@ -10,20 +10,32 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Controller
 public class Handle404Controller implements ErrorController {
 
+    /**
+     * Handles every route different from the ones registered in
+     * the HomeController.
+     * @param model
+     * @return
+     */
     @RequestMapping("/error")
     @ResponseStatus(HttpStatus.OK)
     public String handleError(Model model) {
+        
+        //FIXME: temporary
         boolean new404 = false;
 
+        //FIXME: temporary
         model.addAttribute("url", "/test");
 
+        // New 404 error found
         if (new404) {
             return "new404";
         }
 
+        //FIXME: temporary
         model.addAttribute("username", "admin");
         model.addAttribute("datetime", "13.03.2020 11:06");
 
+        // Already found 404 error
         return "already_found404";
     }
 
--- src/main/java/com/jamais404/HomeController.java
+++ src/main/java/com/jamais404/HomeController.java
@@ -1,19 +1,39 @@
 package com.jamais404;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
+	/**
+	 * Home page
+	 * @return
+	 */
 	@RequestMapping(value = "/")
 	public String home() {
 		return "home";
 	}
 
+	/**
+	 * Search form
+	 * @return
+	 */
 	@RequestMapping(value = "/search", method = RequestMethod.POST)
 	public String search() {
+		// TODO
 		return "home";
+	}
+
+	@RequestMapping(value = "/user/{username}")
+	public String user(Model model) {
+		
+		// TODO
+		model.addAttribute("username", "admin");
+		model.addAttribute("nbFound", "1");
+
+		return "user";
 	}
 }
