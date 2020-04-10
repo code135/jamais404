97d5351ff98fca31cc32902500937b2a4ab267f5
qtipee <quent.vallat@gmail.com>
2020-04-10 17:21:43 +0200

    Adds Jamais404ApplicationTests

---
+++ jamais404/src/test/com/jamais404/com/jamais404/Jamais404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamais404;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jamais404ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
